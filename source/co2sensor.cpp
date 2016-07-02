2e461e9fe34c3e75e1e535866d8fb59439ef3329
Michael Arthur <michael@jumblesoft.co.nz>
2016-07-02 23:08:43 +1200

    clearing up compile issues for co2 code

--- source/co2sensor.cpp
+++ source/co2sensor.cpp
@@ -12,12 +12,12 @@
 #include "clock_config.h"
 #include "board.h"
 
-
+adc16_config_t adc16ConfigStruct;
+adc16_channel_config_t adc16ChannelConfigStruct;
 
 
 void init_co2sensor() {
-	adc16_config_t adc16ConfigStruct;
-	adc16_channel_config_t adc16ChannelConfigStruct;
+
  /*
      * adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
      * adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
@@ -29,22 +29,22 @@ void init_co2sensor() {
      * adc16ConfigStruct.enableLowPower = false;
      * adc16ConfigStruct.enableContinuousConversion = false;
      */
-	-//     adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
-	-//     adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
-	-//     adc16ConfigStruct.enableAsynchronousClock = true;
-	-//     adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
-	-//     adc16ConfigStruct.resolution = kADC16_ResolutionSE8Bit;
-	-//     adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
-	-//     adc16ConfigStruct.enableHighSpeed = false;
-	-//     adc16ConfigStruct.enableLowPower = false;
-	-//     adc16ConfigStruct.enableContinuousConversion = true;
+	//     adc16ConfigStruct.referenceVoltageSource = kADC16_ReferenceVoltageSourceVref;
+	//     adc16ConfigStruct.clockSource = kADC16_ClockSourceAsynchronousClock;
+	//     adc16ConfigStruct.enableAsynchronousClock = true;
+	//     adc16ConfigStruct.clockDivider = kADC16_ClockDivider8;
+	//     adc16ConfigStruct.resolution = kADC16_ResolutionSE8Bit;
+	//     adc16ConfigStruct.longSampleMode = kADC16_LongSampleDisabled;
+	//     adc16ConfigStruct.enableHighSpeed = false;
+	//     adc16ConfigStruct.enableLowPower = false;
+	//     adc16ConfigStruct.enableContinuousConversion = true;
 
 
     ADC16_GetDefaultConfig(&adc16ConfigStruct);
-    ADC16_Init(CO2BASE, &adc16ConfigStruct);
-    ADC16_EnableHardwareTrigger(FLOW_SENSOR_BASE, false); /* Make sure the software trigger is used. */
+    ADC16_Init(CO2_SENSOR_BASE, &adc16ConfigStruct);
+    ADC16_EnableHardwareTrigger(CO2_SENSOR_BASE, false); /* Make sure the software trigger is used. */
 	#if defined(FSL_FEATURE_ADC16_HAS_CALIBRATION) && FSL_FEATURE_ADC16_HAS_CALIBRATION
-		if (kStatus_Success == ADC16_DoAutoCalibration(FLOW_SENSOR_BASE))
+		if (kStatus_Success == ADC16_DoAutoCalibration(CO2_SENSOR_BASE))
 		{
 			PRINTF("ADC16_DoAutoCalibration() Done.\r\n");
 		}
@@ -55,7 +55,7 @@ void init_co2sensor() {
 	#endif /* FSL_FEATURE_ADC16_HAS_CALIBRATION */
 		PRINTF("Press any key to get user channel's ADC value ...\r\n");
 
-		adc16ChannelConfigStruct.channelNumber = FLOW_SENSOR_USER_CHANNEL;
+		adc16ChannelConfigStruct.channelNumber = CO2_SENSOR_USER_CHANNEL;
 		adc16ChannelConfigStruct.enableInterruptOnConversionCompleted = false;
 	#if defined(FSL_FEATURE_ADC16_HAS_DIFF_MODE) && FSL_FEATURE_ADC16_HAS_DIFF_MODE
 		adc16ChannelConfigStruct.enableDifferentialConversion = false;
@@ -65,10 +65,10 @@ void init_co2sensor() {
 }
 
 uint32_t readCO2Sensor() {
-	ADC16_SetChannelConfig(FLOW_SENSOR_BASE, FLOW_SENSOR_CHANNEL_GROUP, &adc16ChannelConfigStruct);
+	ADC16_SetChannelConfig(CO2_SENSOR_BASE, CO2_SENSOR_CHANNEL_GROUP, &adc16ChannelConfigStruct);
 	while (0U == (kADC16_ChannelConversionDoneFlag &
-				  ADC16_GetChannelStatusFlags(FLOW_SENSOR_BASE, FLOW_SENSOR_CHANNEL_GROUP)))
+				  ADC16_GetChannelStatusFlags(CO2_SENSOR_BASE, CO2_SENSOR_CHANNEL_GROUP)))
 	{
 	}
-	PRINTF("ADC Value: %d\r\n", ADC16_GetChannelConversionValue(FLOW_SENSOR_BASE, FLOW_SENSOR_CHANNEL_GROUP));
+	PRINTF("ADC Value: %d\r\n", ADC16_GetChannelConversionValue(CO2_SENSOR_BASE, CO2_SENSOR_CHANNEL_GROUP));
 }
